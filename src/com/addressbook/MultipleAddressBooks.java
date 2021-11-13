223df1dfcb0fc396f4d483b00eb4ada42dd3dffe
Mukul Jain <mukuljain422@gmail.com>
2021-11-13 21:31:24 +0530

    read write in csv file

--- src/com/addressbook/MultipleAddressBooks.java
+++ src/com/addressbook/MultipleAddressBooks.java
@@ -13,11 +13,15 @@ import java.util.stream.Collectors;
 public class MultipleAddressBooks {
 	Scanner scanner = new Scanner(System.in);
 
+	//file path
 	static final String RESOURCE_FILE_PATH = "F:\\RFP-070\\LinecomparisonOOPs\\AddressBookMgmt\\src\\com\\resource\\AddressBookDetails.txt";
+	static final String RESOURCE_CSV_FILE_PATH = "F:\\RFP-070\\LinecomparisonOOPs\\AddressBookMgmt\\src\\com\\resource\\AddressBookDetails.csv";
+	
 	PersonInformation person = new PersonInformation();
 	List<PersonInformation> contactList = new ArrayList<>();
 	HashMap<String, Contact> contactService = new HashMap<>();
 
+	//add address book
 	public void addAddressBook() {
 		System.out.println("Enter Name of new Address Book: ");
 		String bookName = scanner.next();
@@ -35,6 +39,7 @@ public class MultipleAddressBooks {
 		}
 	}
 
+	//add contact address book
 	public void addContact() {
 		System.out.println("Enter the name of Address book to add the contact.");
 		String newContact = scanner.nextLine();
@@ -47,6 +52,7 @@ public class MultipleAddressBooks {
 		}
 	}
 
+	//edit contact in address book
 	public void editContactInBook() {
 		System.out.println("Enter Name to Edit Address Book: ");
 		String editBookName = scanner.next();
@@ -58,6 +64,7 @@ public class MultipleAddressBooks {
 		}
 	}
 
+	//delete address book
 	public void deleteAddressBook() {
 		System.out.println("Enter Name to Delete Address Book: ");
 		String bookName = scanner.next();
@@ -69,6 +76,7 @@ public class MultipleAddressBooks {
 		}
 	}
 
+	//delete contact in address book
 	public void deleteContactInBook() {
 		System.out.println("Enter Name to delete the contacts from Address Book : ");
 		String bookName = scanner.next();
@@ -105,6 +113,29 @@ public class MultipleAddressBooks {
 		}
 	}
 
+	//search contact by state name
+	public void searchByState(String state) {
+		for (Map.Entry<String, Contact> entry : contactService.entrySet()) {
+			List<PersonInformation> contactListByState = entry.getValue().contactList;
+			List<PersonInformation> collect = contactListByState.stream().filter(p -> p.getState().equalsIgnoreCase(state)).collect(Collectors.toList());
+			for (PersonInformation contactsss : collect) {
+				System.out.println("Search result: " + contactsss);
+			}
+		}
+		System.out.println(" ");
+	}
+
+	//search contact by city name
+	public void searchByCity(String city) {
+		for (Map.Entry<String, Contact> entry : contactService.entrySet()) {
+			List<PersonInformation> contactListByCity = entry.getValue().contactList;
+			List<PersonInformation> collect = contactListByCity.stream().filter(p -> p.getCity().equalsIgnoreCase(city)).collect(Collectors.toList());
+			for (PersonInformation contact : collect) {
+				System.out.println("Search result: " + contact);
+			}
+		}
+	}
+
 	//count number of contacts in all address books using city or state name
 	public void countContacts() {
 		while (true) {
@@ -127,6 +158,26 @@ public class MultipleAddressBooks {
 			default:
 				System.out.println("Entered choice is incorrect!.. please enter correct choice");
 			}
+		}
+	}
+
+	//count contact by city name
+	public void countByCity(String cityName) {
+		for (Map.Entry<String, Contact> entry : contactService.entrySet()) {
+			System.out.println("The contacts in the Book of < " + entry.getKey() + " > are!...");
+			List<PersonInformation> contactListByCity = entry.getValue().contactList;
+			long countContactsByCity = contactListByCity.stream().filter(g -> g.getCity().equalsIgnoreCase(cityName)).count();
+			System.out.println("Total Number of Contact from '" + cityName + "' city is " + countContactsByCity);
+		}
+	}
+
+	//count contact by state name
+	public void countByState(String stateName) {
+		for (Map.Entry<String, Contact> entry : contactService.entrySet()) {
+			System.out.println("The contacts in the Book of < " + entry.getKey() + " > are!...");
+			List<PersonInformation> contactListByState = entry.getValue().contactList;
+			long countContactsByState = contactListByState.stream().filter(g -> g.getState().equalsIgnoreCase(stateName)).count();
+			System.out.println("Total Number of Contact from '" + stateName + "' state is" + countContactsByState);
 		}
 	}
 
@@ -157,49 +208,7 @@ public class MultipleAddressBooks {
 		}
 	}
 
-	//search contact by state name
-	public void searchByState(String state) {
-		for (Map.Entry<String, Contact> entry : contactService.entrySet()) {
-			List<PersonInformation> contactListByState = entry.getValue().contactList;
-			List<PersonInformation> collect = contactListByState.stream().filter(p -> p.getState().equalsIgnoreCase(state)).collect(Collectors.toList());
-			for (PersonInformation contactsss : collect) {
-				System.out.println("Search result: " + contactsss);
-			}
-		}
-		System.out.println(" ");
-	}
-
-	//search contact by city name
-	public void searchByCity(String city) {
-		for (Map.Entry<String, Contact> entry : contactService.entrySet()) {
-			List<PersonInformation> contactListByCity = entry.getValue().contactList;
-			List<PersonInformation> collect = contactListByCity.stream().filter(p -> p.getCity().equalsIgnoreCase(city)).collect(Collectors.toList());
-			for (PersonInformation contact : collect) {
-				System.out.println("Search result: " + contact);
-			}
-		}
-	}
-
-	//count contact by city name
-	public void countByCity(String cityName) {
-		for (Map.Entry<String, Contact> entry : contactService.entrySet()) {
-			System.out.println("The contacts in the Book of < " + entry.getKey() + " > are!...");
-			List<PersonInformation> contactListByCity = entry.getValue().contactList;
-			long countContactsByCity = contactListByCity.stream().filter(g -> g.getCity().equalsIgnoreCase(cityName)).count();
-			System.out.println("Total Number of Contact from '" + cityName + "' city is " + countContactsByCity);
-		}
-	}
-
-	//count contact by state name
-	public void countByState(String stateName) {
-		for (Map.Entry<String, Contact> entry : contactService.entrySet()) {
-			System.out.println("The contacts in the Book of < " + entry.getKey() + " > are!...");
-			List<PersonInformation> contactListByState = entry.getValue().contactList;
-			long countContactsByState = contactListByState.stream().filter(g -> g.getState().equalsIgnoreCase(stateName)).count();
-			System.out.println("Total Number of Contact from '" + stateName + "' state is" + countContactsByState);
-		}
-	}
-
+	//sort the contacts by name
 	public void sortByName(){
 		for (Map.Entry<String, Contact> entry : contactService.entrySet()) {
 			System.out.println("The contacts in the Book of < " + entry.getKey() + " > are!...");
@@ -209,6 +218,7 @@ public class MultipleAddressBooks {
 		}
 	}
 
+	//sort the contacts by city
 	public void sortByCity(){
 		for (Map.Entry<String, Contact> entry : contactService.entrySet()) {
 			System.out.println("The contacts in the Book of < " + entry.getKey() + " > are!...");
@@ -218,6 +228,7 @@ public class MultipleAddressBooks {
 		}
 	}
 
+	//sort the contacts by state
 	public void sortByState(){
 		for (Map.Entry<String, Contact> entry : contactService.entrySet()) {
 			System.out.println("The contacts in the Book of < " + entry.getKey() + " > are!...");
@@ -227,6 +238,7 @@ public class MultipleAddressBooks {
 		}
 	}
 
+	//sort the contacts by zip code
 	public void sortByZip(){
 		for (Map.Entry<String, Contact> entry : contactService.entrySet()) {
 			System.out.println("The contacts in the Book of < " + entry.getKey() + " > are!...");
@@ -236,22 +248,28 @@ public class MultipleAddressBooks {
 		}
 	}
 
-	//print name of address books
-	public void printBook() {
-		System.out.println("Address Book Programs are: ");
-		for (Map.Entry<String, Contact> entry : contactService.entrySet()) {
-			System.out.println(entry.getKey());
+	//choose option to read and write csv file
+	public void readAndWriteTextFile() throws IOException {
+		while (true) {
+			System.out.println("Enter\n 1. Read the CSV File\n 2. Write In the CSV File\n 0. for previous menu");
+			int choice = scanner.nextInt();
+			scanner.nextLine();
+			switch (choice) {
+			case 1:
+				writeToFile();
+				break;
+			case 2:
+				readFromFile();
+				break;
+			case 0:
+				return;
+			default:
+				System.out.println("Entered choice is incorrect!.. please enter correct choice");
+			}
 		}
 	}
 
-	public void printContactsInBook() {
-		for (Map.Entry<String, Contact> entry : contactService.entrySet()) {
-			System.out.println("The contacts in the Book of < " + entry.getKey() + " > are!...");
-			System.out.println(entry.getValue().contactList);
-		}
-		System.out.println(" ");
-	}
-
+	//write in the text file
 	public void writeToFile() throws IOException {
 		FileWriter fw = new FileWriter(RESOURCE_FILE_PATH);
 		System.out.println("File Writing Started");
@@ -273,11 +291,81 @@ public class MultipleAddressBooks {
 		fw.close();
 		System.out.println("File Writing Stopped");
 	}
+
+	//read from text file
 	public void readFromFile() throws IOException {
 		FileReader fileReader = new FileReader(RESOURCE_FILE_PATH);
 		Scanner scanfile = new Scanner(fileReader);
 		scanfile.useDelimiter("\\Z");
 		System.out.println(scanfile.next() +" ");
 		scanfile.close();
+	}
+
+	//choose option to read and write csv file
+	public void readAndWriteCSVFile() throws IOException {
+		while (true) {
+			System.out.println("Enter\n 1. Read the CSV File\n 2. Write In the CSV File\n 0. for previous menu");
+			int choice = scanner.nextInt();
+			scanner.nextLine();
+			switch (choice) {
+			case 1:
+				readCSVFile();
+				break;
+			case 2:
+				writeCSVFile();
+				break;
+			case 0:
+				return;
+			default:
+				System.out.println("Entered choice is incorrect!.. please enter correct choice");
+			}
+		}
+	}
+
+	//write in the csv file
+	private void writeCSVFile() throws IOException {
+		FileWriter fw = new FileWriter(RESOURCE_CSV_FILE_PATH);
+		fw.write("Address Book Name,FirstName,LastName,Address,State,Zip,Phone Number,E-mail\n");
+		for (Map.Entry<String, Contact> entry : contactService.entrySet()) {
+			List<PersonInformation> contList = entry.getValue().contactList;
+			for(int i=0;i<contList.size();i++) {
+				fw.write(entry.getKey() +
+						"," + contList.get(i).getFirstName() + 
+						"," + contList.get(i).getLastName() + 
+						"," + contList.get(i).getAddress() +
+						"," + contList.get(i).getCity() +
+						"," + contList.get(i).getState() + 
+						"," + contList.get(i).getZipCode() + 
+						"," + contList.get(i).getMobileNo() +
+						"," + contList.get(i).getEmailId()+"\n");
+			}
+		}
+		fw.close();
+	}
+
+	//read csv file
+	private void readCSVFile() throws IOException {
+		FileReader fr = new FileReader(RESOURCE_CSV_FILE_PATH);
+		Scanner sc = new Scanner(fr);
+		sc.useDelimiter("\\Z");
+		System.out.println(sc.next() +" ");
+		sc.close();
+	}
+
+	//print name of address books
+	public void printBook() {
+		System.out.println("Address Book Programs are: ");
+		for (Map.Entry<String, Contact> entry : contactService.entrySet()) {
+			System.out.println(entry.getKey());
+		}
+	}
+
+	//print contacts of all address book
+	public void printContactsInBook() {
+		for (Map.Entry<String, Contact> entry : contactService.entrySet()) {
+			System.out.println("The contacts in the Book of < " + entry.getKey() + " > are!...");
+			System.out.println(entry.getValue().contactList);
+		}
+		System.out.println(" ");
 	}
 }
